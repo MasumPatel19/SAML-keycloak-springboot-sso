44eed506d1ba520e496a9bdcb24e602551fa3c1d
masum <ifour.masum@gmail.com>
2024-02-21 17:04:27 +0530

    saml-keycloak code

---
+++ README.md
@@ -0,0 +1,18 @@
+﻿# Spring-boot2-Security-SAML2.0-keycloak18
+ 
+ Single Sign-On With SAML 2.0, Spring Boot - Security and Keycloak 18.0.1
+ 
+# Step 1: Start Keycloak
+ docker-compose -f docker-compose-keycloak18.yml up
+
+# Step 2: Spring Boot run project:
+mvn spring-boot:run
+
+# Step 3: Go to:
+http://localhost:8081/
+
+user: **admin**
+password: **admin**
+
+# Detailed description and steps for run project found here: 
+https://jarmx.blogspot.com/2022/09/single-sign-on-with-saml-20-spring-boot.html
---
+++ src/main/java/com/henry/authentication/MvcConfig.java
@@ -0,0 +1,44 @@
+package com.henry.authentication;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Configuration class for MVC settings.
+ */
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Autowired
+    CurrentUserHandlerMethodArgumentResolver currentUserHandlerMethodArgumentResolver;
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+
+    }
+
+    /**
+     * Add resource handlers for static resources.
+     *
+     * @param registry The ResourceHandlerRegistry to register resource handlers with.
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        if (!registry.hasMappingForPattern("/static/**")) {
+            registry.addResourceHandler("/static/**")
+                    .addResourceLocations("/static/");
+        }
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(currentUserHandlerMethodArgumentResolver);
+    }
+
+}
